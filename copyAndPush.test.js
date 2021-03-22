3f9f4f97e57eb901e2e995a5a7680b141d9bbdaa
Cherno Badjan <badjancherno@gmail.com>
2021-03-22 14:50:46 -0700

    test written

---
+++ copyAndPush.test.js
@@ -0,0 +1,9 @@
+describe('copyAndPush function', () => {
+  it('takes in array and returns a new array with a new item pushed in array', () => {
+    const numbers = [1, 2, 3];
+    const newArr = copyAndPush(numbers, 4);
+    const newerArr = [1, 2, 3, 4];
+
+    expect(newArr).toEqual(newerArr);
+  });
+});
