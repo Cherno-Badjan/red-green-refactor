ced9a3d8e8a21569c17ee0cdf13a33f4d9531f75
Cherno Badjan <badjancherno@gmail.com>
2021-03-22 14:05:23 -0700

    RED

---
+++ getName.js
@@ -0,0 +1,7 @@
+const getName = (randomObject) => {
+    const randomObject = {
+    name:'booger',
+    age:'booger',
+    weight:'booger'}
+  return randomObject[name];
+};
---
+++ getName.test.js
@@ -0,0 +1,8 @@
+describe('getName function', () => {
+  it('takes in object and returns value of name property', () => {
+    const spot = { name: 'spot', age: 5, weight: '20 lbs' };
+
+    const nameProp = getName(spot);
+
+  });
+});
